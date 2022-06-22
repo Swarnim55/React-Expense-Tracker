229e34d87ecda5af66620a7cc9f21a9217d7c788
Your Name Swarnim 55 <swarnimpaudyal55@gmail.com>
2022-06-22 16:47:52 +0545

    Added Form Inputs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 const App = () => {
   const expenses = [
     { title: "Transportation", amount: 60, date: new Date(2022, 5, 17) },
@@ -11,7 +12,7 @@ const App = () => {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
